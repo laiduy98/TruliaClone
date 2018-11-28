2aeb1e056aff5330b6aa34b34895ad6f208f1d48
Lai Khang Duy <laiduy98@gmail.com>
2018-11-28 22:38:36 +0700

    merge an

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    
+?>
+ 
+<?php
+    require 'header.php';
+?>
+
+
+<?php
+    require 'navbarJumbotron.php';
+?>
+
+
+
+ <?php
+    require 'OurHtml/about.html';
+?>
+
+
+
+
+<?php
+    require 'footer.php';
+?>
+
+
+
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<link rel="icon" href="images/logoblack.png">
     <title>USTH</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,9 +10,13 @@
     <link rel="stylesheet" href="OurCss/ourStyle.css">
     <link rel="stylesheet" href="OurCss/signup.css">
     <script src="jquery-3.3.1.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="OurJs/OurScript.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    
 
 </head>
 
